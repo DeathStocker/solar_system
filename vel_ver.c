169f5363bd834247240b63b30020420f35aacb44
DeathStocker <utkarsh2602@gmail.com>
2016-08-21 19:41:40 +0530

    Added user inputs for the spring constant, and the mass of the particle.

--- vel_ver.c
+++ vel_ver.c
@@ -15,16 +15,22 @@ int main(int argc, char* argv[])
 	double t;
 	double delta_t;
 
-	printf("Enter the final time of the trajectory = ");
+	printf("Enter the final time (t) of the trajectory = ");
 	scanf("%lf", &t);
 	printf("Enter Δt = ");
 	scanf("%lf", &delta_t);
 
 	double x0, v0;
-	printf("Enter the initial position of the particle = ");
+	printf("Enter the initial position (x0) of the particle = ");
 	scanf("%lf", &x0);
-	printf("Enter the initial velocity of the particle = ");
+	printf("Enter the initial velocity (y0) of the particle = ");
 	scanf("%lf", &v0);
+
+	double k, m;
+	printf("Enter the spring constant (k) = ");
+	scanf("%lf", &k);
+	printf("Enter the mass (m) of the particle = ");
+	scanf("%lf", &m);
 
 	int no_of_iter = t / delta_t;
 
