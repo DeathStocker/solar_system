9be8dd2bcd33086ed7f306034f6c74603784be5d
DeathStocker <DeathStocker@users.noreply.github.com>
2016-08-21 17:34:48 +0530

    Added naive calculus implementations.
    Naive implementations of basic differentiation and integration with limits,
    a function to find the slope between two points in 2-D.
    Also a custom round function for rounding off values to try to eliminate
    floating point errors in the calculations due to iterative addition operations.

---
+++ calc.h
@@ -0,0 +1,21 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct pt_2d {
+	double x;
+	double y;
+} point_2d;
+
+double find_slope(point_2d a, point_2d b);
+
+double diff(double (*f)(double), double x, double delta);
+
+double integrate(double (*f)(double), double x1, double x2, double delta);
+
+double my_round(double x, unsigned int digits);
+
+#endif
